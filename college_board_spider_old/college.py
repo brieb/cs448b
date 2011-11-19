184aa74db02b7bdfc19927128295778630bc760b
Brie <bbunge7@gmail.com>
2011-11-18 23:35:19 -0800

    python scraper

---
+++ college_board_spider_old/college.py
@@ -0,0 +1,17 @@
+from scrapy.item import Item, Field
+
+class College(Item):
+  address
+  url
+  type_of_school
+  calendar
+  degrees_offered
+  setting
+  size_total_undergrads
+  size_grad_enrollment
+  size_student_faculty_ratio
+  student_body
+  admission_office
+  acceptance_rate
+  percent_soph_return
+
