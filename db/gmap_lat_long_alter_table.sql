94966e6ee39cb8a2d64cc107bec6c54d2d336208
Brie <bbunge7@gmail.com>
2011-12-08 09:08:18 -0800

    mockups and lat lng to is_usa

--- db/gmap_lat_long_alter_table.sql
+++ db/gmap_lat_long_alter_table.sql
@@ -1,3 +1,5 @@
 alter table college add column latitude float default null;
 alter table college add column longitude float default null;
 alter table college add column gmap_lat_long_address text;
+alter table college add column gmap_country text;
+alter table college add column is_usa integer default 0;
