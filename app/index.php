1b564f8bc513a6ec4e6fdcbcafb0377fc3bd2a7d
Brie <bbunge7@gmail.com>
2011-12-13 19:59:35 -0800

    analytics

--- app/index.php
+++ app/index.php
@@ -25,6 +25,22 @@
 
     <script src="js/map_college_to_data_index.js"></script>
     <script src="js/script.js"></script>
+
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-27701363-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+
+
   </head>
   <body>
 
